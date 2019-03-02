8d1abd44e45a12970bc4cc024b6df90b82339b87
Shyam <shyampanchal95@gmail.com>
2019-03-02 18:33:00 -0500

    Recreated API

---
+++ DalalStreetAPI/DalalStreetAPI/Models/DS_Context.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DalalStreetAPI.Models
+{
+    public partial class DS_Context : DbContext
+    {
+        public DS_Context() { }
+
+        public DS_Context(DbContextOptions<DS_Context> options) : base(options) { }
+
+
+        public virtual DbSet<DS_Company> DS_Company { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured){ }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DS_Company>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Name).IsRequired();
+
+                entity.Property(e => e.stockValues).IsRequired();
+            });
+        }
+    }
+}
---
+++ DalalStreetAPI/DalalStreetAPI/Services/IDS_Service.cs
@@ -0,0 +1,20 @@
+﻿using DalalStreetAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DalalStreetAPI.Services
+{
+    public interface IDS_Service
+    {
+        #region DS_Company
+        Task AddAsync(DS_Company record);
+        Task AddRecordAsync(IEnumerable<DS_Company> records);
+        Task<DS_Company> RemoveAsync(int id);
+        Task<IEnumerable<DS_Company>> GetAllAsync();
+        Task<DS_Company> FindAsync(int id);
+        Task UpdateAsync(DS_Company record);
+        #endregion
+    }
+}
